942aee66a5f1826276d6c2c8a3a7fdb56cf651ac
Allega <mr.oger@mail.ru>
2018-08-10 00:18:22 +0300

    Book store

---
+++ BookingAppStore/BookingAppStore/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BookingAppStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingAppStore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // for communication with db
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // get our book list
+            var books = db.Books;
+
+            // transfer in views
+            ViewBag.Books = books;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BookingAppStore/BookingAppStore/Models/BookContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookingAppStore.Models
+{
+    public class BookContext : DbContext
+    {
+        // for communication with db
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    // initialize our db first parameters
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        // DropCreateDatabaseAlways - recreates this db all time we start our app
+
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "The Adventures of Sherlock Holmes", Author = "Arhur Conan Doyle", Price = 220 });
+            db.Books.Add(new Book { Name = "A Tale of Two Cities", Author = "Charles Dickens", Price = 180 });
+            db.Books.Add(new Book { Name = "Man's Search for Meaning", Author = "Viktor E Frankl", Price = 150 });
+
+            base.Seed(db);
+        }
+    }
+}
