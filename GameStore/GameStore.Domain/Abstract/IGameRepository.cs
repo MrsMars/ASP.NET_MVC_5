ba281d3cf47f18396f728daf11ef26ee51169c2d
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-22 18:24:37 +0300

    create new view and master page

--- GameStore/GameStore.Domain/Abstract/IGameRepository.cs
+++ GameStore/GameStore.Domain/Abstract/IGameRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameStore.Domain.Entities;
 
 namespace GameStore.Domain.Abstract
 {
-    interface IGameRepository
+    public interface IGameRepository
     {
+        IEnumerable<Game> Games { get; }
     }
 }
