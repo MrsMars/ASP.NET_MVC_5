d236223eb8829b8fb087ff7d5ace674c1fa75525
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-23 11:26:25 +0300

    complete error with sql version and db views

--- GameStore/GameStore.Domain/Concrete/EFGameRepository.cs
+++ GameStore/GameStore.Domain/Concrete/EFGameRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
 
 namespace GameStore.Domain.Concrete
 {
-    class EFGameRepository
+    public class EFGameRepository : IGameRepository
     {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Game> Games { get { return context.Games; } }
     }
 }
