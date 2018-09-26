526993a3c31627172262a094d8600a070181621b
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-26 11:23:46 +0300

    add some unit tests

---
+++ GameStore/GameStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder
+    {
+    }
+}
