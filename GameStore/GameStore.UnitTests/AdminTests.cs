8926a0342407425a828c383474e0fef40afb24da
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-29 13:16:38 +0300

    add admin controller and view. add "edit" method and unit test

---
+++ GameStore/GameStore.UnitTests/AdminTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GameStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Games()
+        {
+            // arrange
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game{ GameId = 1, Name = "Game_1" },
+                new Game{ GameId = 2, Name = "Game_2" },
+                new Game{ GameId = 3, Name = "Game_3" },
+                new Game{ GameId = 4, Name = "Game_4" },
+                new Game{ GameId = 5, Name = "Game_5" }
+            });
+
+            AdminController controller = new AdminController(mock.Object);
+
+            // act
+            List<Game> result = ((IEnumerable<Game>)controller.Index().ViewData.Model).ToList();
+
+            // assert
+            Assert.AreEqual(result.Count(), 5);
+            Assert.AreEqual("Game_1", result[0].Name);
+            Assert.AreEqual("Game_2", result[1].Name);
+            Assert.AreEqual("Game_3", result[2].Name);
+        }
+
+        [TestMethod]
+        public void Can_Edit_Game()
+        {
+            // arrange
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game{ GameId = 1, Name = "Game_1" },
+                new Game{ GameId = 2, Name = "Game_2" },
+                new Game{ GameId = 3, Name = "Game_3" },
+                new Game{ GameId = 4, Name = "Game_4" },
+                new Game{ GameId = 5, Name = "Game_5" }
+            });
+
+            AdminController controller = new AdminController(mock.Object);
+
+            // act
+            Game game_1 = controller.Edit(1).ViewData.Model as Game;
+            Game game_3 = controller.Edit(2).ViewData.Model as Game;
+            Game game_2 = controller.Edit(3).ViewData.Model as Game;
+
+            // assert
+            Assert.AreEqual(1, game_1.GameId);
+            Assert.AreEqual(2, game_2.GameId);
+            Assert.AreEqual(3, game_3.GameId);
+        }
+
+        [TestMethod]
+        public void Cannot_Edit_Nonexistent_Game()
+        {
+            // arrange
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game{ GameId = 1, Name = "Game_1" },
+                new Game{ GameId = 2, Name = "Game_2" },
+                new Game{ GameId = 3, Name = "Game_3" },
+                new Game{ GameId = 4, Name = "Game_4" },
+                new Game{ GameId = 5, Name = "Game_5" }
+            });
+
+            AdminController controller = new AdminController(mock.Object);
+
+            // act
+            Game result = controller.Edit(6).ViewData.Model as Game;
+
+            // assert
+        }
+    }
+}
---
+++ GameStore/GameStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IGameRepository repository;
+
+        public AdminController (IGameRepository repo) { repository = repo; }
+
+        public ViewResult Index() { return View(repository.Games); }
+
+        public ViewResult Edit(int gameId)
+        {
+            Game game = repository.Games.FirstOrDefault(g => g.GameId == gameId);
+            return View(game);
+        }
+    }
+}
